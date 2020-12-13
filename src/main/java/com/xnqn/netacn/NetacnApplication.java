f9a0f9b0bc5d7732ba13bddea1dd045e3f854bce
79479334 <79479334@qq.com>
2020-12-13 13:52:53 +0800

    创建项目

---
+++ src/main/java/com/xnqn/netacn/NetacnApplication.java
@@ -0,0 +1,13 @@
+package com.xnqn.netacn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetacnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetacnApplication.class, args);
+    }
+
+}
