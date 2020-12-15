3a77e2eddda0d37957e750e49c5fbb38a4f1bdb2
79479334 <79479334@qq.com>
2020-12-15 16:55:24 +0800

    2020-12-15 lombok

--- src/main/java/com/xnqn/netacn/model/UserInfo.java
+++ src/main/java/com/xnqn/netacn/model/UserInfo.java
@@ -1,7 +1,9 @@
 package com.xnqn.netacn.model;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.util.Date;
+@Data
 public class UserInfo {
     private Long id;
 
@@ -19,67 +21,4 @@ public class UserInfo {
 
     private Integer isUnlimit;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getGmtCreate() {
-        return gmtCreate;
-    }
-
-    public void setGmtCreate(Date gmtCreate) {
-        this.gmtCreate = gmtCreate;
-    }
-
-    public Date getGmtModified() {
-        return gmtModified;
-    }
-
-    public void setGmtModified(Date gmtModified) {
-        this.gmtModified = gmtModified;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public void setAccount(String account) {
-        this.account = account == null ? null : account.trim();
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password == null ? null : password.trim();
-    }
-
-    public String getHeadImg() {
-        return headImg;
-    }
-
-    public void setHeadImg(String headImg) {
-        this.headImg = headImg == null ? null : headImg.trim();
-    }
-
-    public Integer getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(Integer language) {
-        this.language = language;
-    }
-
-    public Integer getIsUnlimit() {
-        return isUnlimit;
-    }
-
-    public void setIsUnlimit(Integer isUnlimit) {
-        this.isUnlimit = isUnlimit;
-    }
 }
