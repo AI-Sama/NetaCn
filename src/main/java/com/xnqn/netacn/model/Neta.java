35a466e36c0a1537569219997dd91980705a2e8c
79479334 <79479334@qq.com>
2020-12-28 16:36:09 +0800

    2020-12-28

--- src/main/java/com/xnqn/netacn/model/Neta.java
+++ src/main/java/com/xnqn/netacn/model/Neta.java
@@ -1,6 +1,7 @@
 package com.xnqn.netacn.model;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
@@ -13,33 +14,33 @@ public class Neta {
     private Date gmtCreate;
 
     private Date gmtModified;
-
+    @ApiModelProperty("词语")
     private String netaWord;
-
+    @ApiModelProperty("上传人")
     private String upUser;
-
+    @ApiModelProperty("拼音，多个用空格隔开")
     private String pinyin;
-
+    @ApiModelProperty("假名注音")
     private String katakana;
-
+    @ApiModelProperty("标签，多个用|隔开")
     private String netaLabel;
-
+    @ApiModelProperty("是否限制，默认1限制")
     private Byte wordShield;
-
+    @ApiModelProperty("出现日期")
     private Integer netaDate;
-
+    @ApiModelProperty("中文释义")
     private String cnExplanation;
-
+    @ApiModelProperty("日语释义")
     private String jpExplanation;
-
+    @ApiModelProperty("状态")
     private Byte netaStatus;
-
+    @ApiModelProperty("点赞数")
     private Integer likeNum;
-
+    @ApiModelProperty("观看数")
     private Integer lookNum;
-
+    @ApiModelProperty("收藏数")
     private Integer starNum;
-
+    @ApiModelProperty("拒绝原因")
     private String reason;
 
     private String spare1;
