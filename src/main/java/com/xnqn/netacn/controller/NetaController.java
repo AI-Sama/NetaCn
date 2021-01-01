212eec82047071610f375c3c71d83fdace13af0d
79479334 <79479334@qq.com>
2021-01-01 17:03:23 +0800

    2021-01-01

--- src/main/java/com/xnqn/netacn/controller/NetaController.java
+++ src/main/java/com/xnqn/netacn/controller/NetaController.java
@@ -1,8 +1,10 @@
 package com.xnqn.netacn.controller;
 
+import com.xnqn.netacn.service.impl.NetaImpl;
 import com.xnqn.netacn.utils.ResultBean;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Api("词语模块")
 @RequestMapping("/neta")
 public class NetaController {
-
+    @Autowired
+    NetaImpl netaimpl;
     @ApiOperation("test")
     @GetMapping(value = "/test", produces = {"application/json;charset=utf-8"})
     public ResultBean test() {
