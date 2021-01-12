ef50a6a0e3ecfeda140868ae506779930ce26b61
79479334 <79479334@qq.com>
2021-01-12 21:47:28 +0800

    2021-01-12

--- src/main/java/com/xnqn/netacn/interceptor/UserInterceptor.java
+++ src/main/java/com/xnqn/netacn/interceptor/UserInterceptor.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
  * @Description: 用户拦截器
  */
 public class UserInterceptor implements HandlerInterceptor {
+    
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 //        if("OPTIONS".equals(request.getMethod().toUpperCase())) {
