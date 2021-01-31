ff0d173694e5933ceb7028a7151a7b36eb2b767d
79479334 <79479334@qq.com>
2021-01-31 18:34:57 +0800

    2021-01-31

--- src/main/java/com/xnqn/netacn/controller/ErrorController.java
+++ src/main/java/com/xnqn/netacn/controller/ErrorController.java
@@ -18,6 +18,7 @@ import java.util.Date;
 @Api(tags ="错误控制器")
 @RequestMapping("/error")
 public class ErrorController {
+    
     @ApiOperation("token校验失败")
     @GetMapping(value = "/errorToken", produces = {"application/json;charset=utf-8"})
     public ResultBean errorToken(HttpServletRequest httpServletRequest) {
